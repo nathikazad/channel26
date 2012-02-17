b5c139cca70aad6296eb818ebfe29bd5d9d30c7e
Arnesh <arneshmudaliar@gmail.com>
2012-02-16 22:41:57 -0800

    login_1

--- db/migrate/20120214081412_create_students.rb
+++ db/migrate/20120214081412_create_students.rb
@@ -3,6 +3,9 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
+      t.string :username
+      t.string :password
+      t.string :email
       t.string :major
       t.string :CellPhone 
       t.timestamps
