e45433f09dd469dc4ffd548e5002e0e91c96d551
Nathik Azad <nahikazad@gmail.com>
2012-02-20 16:03:44 -0800

    twilio integration 6

--- app/controllers/twiliorespp_controller.rb
+++ app/controllers/twiliorespp_controller.rb
@@ -3,7 +3,7 @@ class TwilioresppController < ApplicationController
     msg=params["Body"]
     number=params["From"]
     student=Student.find_by_CellPhone(number)
-    response = student.first_name + student.last_name + "you are a fag"
+    response = "#{student.first_name + student.last_name}" + ", you are a fag"
     twilio_sid = "ACfa79fe45769b4f4da3e379adbd6dae18"
     twilio_token = "6902d3f2f1648e586f8733dc849c1ef4"
     
