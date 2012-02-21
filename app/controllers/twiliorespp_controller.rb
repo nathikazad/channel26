8df9239d54e123bc81139236cfdc081976f9cb28
Nathik Azad <nahikazad@gmail.com>
2012-02-20 16:09:54 -0800

    twilio integration 11

--- app/controllers/twiliorespp_controller.rb
+++ app/controllers/twiliorespp_controller.rb
@@ -20,7 +20,7 @@ class TwilioresppController < ApplicationController
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
     @twilio_client.account.sms.messages.create(
       :from => "4155992671",
-      :to => number_to_send_to,
+      :to => params["From"],
       :body => response
     ) 
     
