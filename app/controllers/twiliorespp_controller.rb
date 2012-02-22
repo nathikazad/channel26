6d53a108b06300214f31b48e6ed3ac1481b88ee2
Nathik Azad <nahikazad@gmail.com>
2012-02-21 23:02:32 -0800

    twilio integration.xml

--- app/controllers/twiliorespp_controller.rb
+++ app/controllers/twiliorespp_controller.rb
@@ -2,7 +2,10 @@ class TwilioresppController < ApplicationController
   def answerMachine
     @msg=params["Body"]
     number=params["From"]
-   
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml # index.xml.builder
+    end
     
   end
 
