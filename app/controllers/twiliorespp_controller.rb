9b2d0ac060ceb03c8aa2e7d97d5094a58a8dcbd3
Nathik Azad <nahikazad@gmail.com>
2012-02-21 22:56:01 -0800

    twilio integration.xml

--- app/controllers/twiliorespp_controller.rb
+++ app/controllers/twiliorespp_controller.rb
@@ -1,27 +1,8 @@
 class TwilioresppController < ApplicationController
   def answerMachine
-    msg=params["Body"]
+    @msg=params["Body"]
     number=params["From"]
-    student=Student.find_by_CellPhone(number)
-    response = "#{student.first_name} #{student.last_name}, you are a fag"
-    twilio_sid = "ACfa79fe45769b4f4da3e379adbd6dae18"
-    twilio_token = "6902d3f2f1648e586f8733dc849c1ef4"
-    
-    #authenticate
-    
-    #Find when & duration#
-    
-    #Find classname
-    
-    #Find type & number
-    
-    
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-    @twilio_client.account.sms.messages.create(
-      :from => "4155992671",
-      :to => number,
-      :body => response
-    ) 
+   
     
   end
 
