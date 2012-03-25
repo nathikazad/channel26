f9c17899a4735ed2167ac95a9ded3f6e0c1b8bd2
Nathik Azad <nahikazad@gmail.com>
2012-03-25 01:59:48 -0700

    text me any question nigggaaas

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
-  attr_accessible :user_name,:password,:leftoverSMS
+  attr_accessible :user_name,:password, :leftoverSMS
   has_and_belongs_to_many :channels
   has_many :grades
+  has_many :posts ,:as => :poster, :dependent => :destroy
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,5 @@
 class Teacher < ActiveRecord::Base
   has_many :channels, :as => :leader 
   has_many :simwords, :as => :simlable, :dependent => :destroy
+  has_many :posts ,:as => :poster, :dependent => :destroy
 end
