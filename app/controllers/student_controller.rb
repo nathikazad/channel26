3e0fad2a8fbd9967ae0050e6bb145a07260b535e
Nathik Azad <nahikazad@gmail.com>
2012-03-25 05:02:40 -0700

    all

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -235,6 +235,9 @@ class StudentController < ApplicationController
               if(@array[@i].eql?("next"))
                 end_date=end_date+30
                 limit=1
+              elsif(@array[@i].eql?("all"))
+                end_date=end_date+30
+                limit=20
               end
               classrooms=classrooms | queryThat(channels,j,nil,start_date,end_date,limit)
             end
